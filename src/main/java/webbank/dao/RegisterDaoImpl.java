bb982ad34cb4e1a4aaf6711b715eacebd4a71e7c
gabor <kovacsgaboo@gmail.com>
2018-10-14 00:33:35 +0200

    test list

--- src/main/java/webbank/dao/RegisterDaoImpl.java
+++ src/main/java/webbank/dao/RegisterDaoImpl.java
@@ -14,18 +14,8 @@ public class RegisterDaoImpl implements RegisterDao {
 	private List<Register> dummyList = new ArrayList<Register>();
 
 	public RegisterDaoImpl() {
-		dummyList.add(new Register("01", "Architecture"));
-		dummyList.add(new Register("01001", "Architectural Engineering"));
-		dummyList.add(new Register("01002", "Architectural History and Criticism"));
-		dummyList.add(new Register("02", "Arts and Humanities"));
-		dummyList.add(new Register("02001", "African Languages and Societies"));
-		dummyList.add(new Register("02002", "American Studies"));
-		dummyList.add(new Register("02002001", "American Film Studies"));
-		dummyList.add(new Register("02002002", "American Literature"));
-		dummyList.add(new Register("02002003", "American Material Culture"));
-		dummyList.add(new Register("02003", "Appalachian Studies"));
-		dummyList.add(new Register("03", "Business"));
-		dummyList.add(new Register("03001", "Accounting"));
+		dummyList.add(new Register("01", "test"));
+
 	}
 
 	public List<Register> getRegister() {
